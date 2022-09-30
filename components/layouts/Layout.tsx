56c94dd0a9d542dc2ef014fffb885d6ba88c8f90
abrahag40 <abrahag40@gmail.com>
2022-09-30 09:06:34 -0500

    creating proyect

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import Head from "next/head"
+
+import { Box } from "@mui/material"
+import { Navbar, Sidebar } from "../ui";
+
+interface Props {
+  title?: string;
+  children: any;
+}
+
+const Layout: any = ({ title = 'OpenJira', children }: Props) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+      <Head>
+        <title> { title } </title>
+      </Head>
+      <Navbar />
+      <Sidebar />
+      <Box sx={{ padding: '10px 20px' }}>
+        { children }
+      </Box>
+    </Box>
+  )
+}
+
+export default Layout
